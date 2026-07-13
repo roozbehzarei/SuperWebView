6f842b4d44dc7011d239caa16a3f1cf962b1134e
Roozbeh Zarei <r.zarei@oticamp.ir>
2026-07-13 15:08:26 +0330

    Update Gradle build configuration to add support for Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,18 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.roozbehzarei.superwebview"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.roozbehzarei.superwebview"
-        minSdk = 23
-        targetSdk = 36
+        minSdk = 24
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -31,41 +26,28 @@ android {
     }
 
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
-    // Compose Bill of Materials
-    val composeBom = platform("androidx.compose:compose-bom:2025.08.01")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-    // Compose dependencies
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.material)
+    debugImplementation(libs.androidx.ui.tooling)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.foundation)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.swiperefreshlayout)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.tooling.preview)
-    debugImplementation(libs.androidx.ui.tooling)
-    // Core
-    implementation(libs.androidx.core.ktx)
-    // Webkit
     implementation(libs.androidx.webkit)
-    // SplashScreen
-    implementation(libs.androidx.core.splashscreen)
-    // Swiperefreshlayout
-    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.activity.compose)
 }
